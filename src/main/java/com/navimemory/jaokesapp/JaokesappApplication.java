560845f1786868fa0bb48606ff900638540d9968
Navnath Reshmaji Chinchore <nath.chinchore@gmail.com>
2020-08-14 18:07:12 +0530

    initial commit

---
+++ src/main/java/com/navimemory/jaokesapp/JaokesappApplication.java
@@ -0,0 +1,13 @@
+package com.navimemory.jaokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaokesappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaokesappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/navimemory/jaokesapp/JaokesappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navimemory.jaokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaokesappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
